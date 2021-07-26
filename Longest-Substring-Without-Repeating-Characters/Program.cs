f8556a616f3aabf51ab4c30772ef2764129d44e1
Nate Tryon <natetryon@gmail.com>
2021-07-26 07:31:13 -0400

    Starting longest substring

---
+++ Longest-Substring-Without-Repeating-Characters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Longest_Substring_Without_Repeating_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int LengthOfLongestSubstring(string s) {
+
+            int result = 0;
+
+
+            return result;
+        }
+    }
+}
