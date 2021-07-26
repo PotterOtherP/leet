aa53b7a97ff457d3bfb31eb513da1ef4d2689e30
Nate Tryon <natetryon@gmail.com>
2021-07-26 06:44:59 -0400

    Add-Two_Numbers solution

--- Add-Two-Numbers/Program.cs
+++ Add-Two-Numbers/Program.cs
@@ -6,14 +6,57 @@ namespace Add_Two_Numbers
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+
+            ListNode l1 = new ListNode(9);
+            l1.Add(9);
+            l1.Add(9);
+            l1.Add(9);
+            l1.Add(9);
+            l1.Add(9);
+            l1.Add(9);
+            l1.PrintNode();
+
+            ListNode l2 = new ListNode(9);
+            l2.Add(9);
+            l2.Add(9);
+            l2.Add(9);
+            l2.PrintNode();
+
+            ListNode answer = AddTwoNumbers(l1, l2);
+            answer.PrintNode();
         }
 
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
 
-            return null;
+            ListNode index = new ListNode();
+            ListNode answer = index;
+
+            while (l1 != null || l2 != null)
+            {
+                int val1 = (l1 != null)? l1.val : 0;
+                int val2 = (l2 != null)? l2.val : 0;
+
+                index.val += val1 + val2;
+
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+
+                if (index.val > 9)
+                {
+                    index.val %= 10;
+                    index.next = new ListNode(1);
+                }
+
+                else if (l1 != null || l2 != null)
+                {
+                    index.next = new ListNode();
+                }
+
+                index = index.next;
+            }
+
+            return answer;
         }
 
     }
@@ -27,6 +70,32 @@ namespace Add_Two_Numbers
 
             this.val = val;
             this.next = next;
+        }
+
+        public void Add(int val) {
+            ListNode last = this;
+            while(last.next != null)
+            {
+                last = last.next;
+            }
+
+            last.next = new ListNode(val);
+        }
+
+        public void PrintNode() {
+
+            ListNode index = this.next;
+            
+            Console.Write($"[{val}");
+
+            while (index != null)
+            {
+                Console.Write($", {index.val}");
+                index = index.next;
+            }
+
+            Console.Write("]\n");
+
         }
     }
 }
