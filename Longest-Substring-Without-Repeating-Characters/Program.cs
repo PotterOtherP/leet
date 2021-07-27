95a429dfc7149d98828f8b7064eebc4f2c5c938a
Nate Tryon <natetryon@gmail.com>
2021-07-27 07:02:02 -0400

    Longest substring solution

--- Longest-Substring-Without-Repeating-Characters/Program.cs
+++ Longest-Substring-Without-Repeating-Characters/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Longest_Substring_Without_Repeating_Characters
 {
@@ -6,15 +7,49 @@ namespace Longest_Substring_Without_Repeating_Characters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input1 = " ";
+
+            int x = LengthOfLongestSubstring(input1);
         }
 
-        public static int LengthOfLongestSubstring(string s) {
+        public static int LengthOfLongestSubstring(string str) {
 
-            int result = 0;
+            Console.WriteLine($"Input: {str}");
 
+            List<char> UsedCharacters = new List<char>();
 
-            return result;
+            if (str == null || str.Length < 1)
+            {
+                return 0;
+            }
+
+            int max = 0;
+            int count = 0;
+
+            for (int i = 0; i < str.Length; ++i)
+            {
+                Console.Write($"{i}: ");
+
+                if (!UsedCharacters.Contains(str[i]))
+                {
+                    ++count;
+                    if (count > max) max = count;
+                    UsedCharacters.Add(str[i]);
+                    Console.WriteLine($"New character: {str[i]}");
+                }
+                else
+                {
+                    if (count > max) max = count;
+                    i = str.Substring(0, i).LastIndexOf(str[i]);
+                    count = 0;
+                    UsedCharacters.Clear();
+                    Console.WriteLine($"Found repeat '{str[i]}', max is {max}, returning to position {i}");
+                }
+
+            }
+
+            Console.WriteLine($"Final answer: {max}");
+            return max;
         }
     }
 }
