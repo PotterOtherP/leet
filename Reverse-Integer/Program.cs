3ba08e794e54797c83a89e236446ef1c7f16593c
Nate Tryon <natetryon@gmail.com>
2021-08-23 07:40:58 -0400

    Reverse Integer solution

---
+++ Reverse-Integer/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+    Given a signed 32-bit integer x, return x with its digits reversed.
+    If reversing x causes the value to go outside the signed 32-bit integer range
+    [-2^31, 2^31 - 1], then return 0.
+
+    Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+    https://leetcode.com/problems/reverse-integer/
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Reverse_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max Int32: " + Int32.MaxValue);
+            Console.WriteLine("Min Int32: " + Int32.MinValue);
+
+            int[] tests = {123, -123, 120, 0, 1534236469};
+
+            foreach (int test in tests)
+            {
+                int res = Reverse(test);
+                Console.WriteLine($"Input: {test} ... reversed: {res}");
+            }
+
+        }
+
+        public static int Reverse(int x) {
+
+            if (Math.Abs(x) < 10) return x;
+
+            int result = 0;
+            bool negative = (x < 0);
+            int originalX = x;
+            x = Math.Abs(x);
+
+            List<int> digits = new List<int>();
+
+            while (x > 0)
+            {
+                digits.Add(x % 10);
+                x /= 10;
+            }
+
+            if (digits.Count == 10 && digits[0] > 2) return 0;
+
+            for (int i = 0; i < digits.Count; ++i)
+            {
+                int next = digits[i] * PowerTen(digits.Count - 1 - i);
+                result += next;
+                if (result < 0) return 0;
+            }
+
+            if (negative) result *= -1;
+
+            return result;
+        }
+
+        public static int PowerTen(int n) {
+
+            if (n == 0) return 1;
+
+            else return 10 * PowerTen(n - 1);
+        }
+    }
+}
--- Rotate-Array/Program.cs
+++ Rotate-Array/Program.cs
@@ -1,12 +1,112 @@
 ﻿using System;
 
+// Given an array, rotate the array to the right by k steps,
+// where k is non-negative.
+
 namespace Rotate_Array
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 2;
+
+            Console.WriteLine($"k = {k}");
+            PrintArray(nums);
+            RotateThree(nums, k);
+        }
+
+        public static void Rotate(int[] nums, int k) {
+
+            int len = nums.Length;
+            if (len == 0) return;
+
+            k = k % len;
+            if (k == 0) return;
+
+            int[] result = new int[len];
+
+            for (int i = 0; i < k; ++i)
+            {
+                result[i] = nums[len - k + i];
+            }
+
+            for (int i = k; i < len; ++i)
+            {
+                result[i] = nums[i - k];
+            }
+
+            nums = result;
+            PrintArray(nums);
+
+        }
+
+        public static void RotateTwo(int[] nums, int k) {
+
+            int len = nums.Length;
+            if (len == 0) return;
+
+            k = k % len;
+            if (k == 0) return;
+
+            int[] extra = new int[k];
+
+            for (int i = 0; i < k; ++i)
+            {
+                extra[i] = nums[len - k + i];
+            }
+
+            for (int i = len - 1; i >= k; --i)
+            {
+                if (k <= i)
+                    nums[i] = nums[i - k];
+                else
+                    nums[i] = nums[i - k + len];
+            }
+
+            for (int i = 0; i < k; ++i)
+            {
+                nums[i] = extra[i];
+            }
+
+
+            PrintArray(extra);
+            PrintArray(nums);
+        }
+
+        public static void RotateThree(int[] nums, int k) {
+
+            int len = nums.Length;
+            if (len == 0) return;
+
+            k = k % len;
+            if (k == 0) return;
+
+            for (int i = 0; i < k; ++i)
+            {
+                
+
+
+            }
+
+            PrintArray(nums);
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            string str = "[";
+            int len = arr.Length;
+            for (int i = 0; i < len - 1; ++i)
+            {
+                str += (arr[i]);
+                str += ", ";
+            }
+
+            str += arr[len - 1];
+            str += "]";
+
+            Console.WriteLine(str);
         }
     }
 }
