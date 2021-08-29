690a34858df2de73d4c3fa0eafe315b6c7d8fcdc
Nate Tryon <natetryon@gmail.com>
2021-08-29 05:06:17 -0400

    Maximum Subarray solution

---
+++ Contains-Duplicate/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contains_Duplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input1 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+            PrintArray(input1);
+            Console.WriteLine(Solution2(input1));
+
+
+        }
+
+        public static void PrintArray(int[] arr) {
+            Console.Write("[");
+            for (int i = 0; i < arr.Length - 1; ++i)
+            {
+                Console.Write(arr[i]);
+                Console.Write(", ");
+            }
+
+            Console.Write(arr[arr.Length - 1]);
+            Console.Write("]\n");
+        }
+
+        public static bool Solution(int[] nums) {
+
+            if (nums.Length <= 1) return false;
+
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                int nextElement = nums[i];
+                int nextPosition = 0;
+                
+                while (nums[nextPosition] < nextElement)
+                {
+                    ++nextPosition;
+                }
+                
+                if (nextPosition == i)
+                    continue;
+                
+                else if (nextElement == nums[nextPosition])
+                    return true;
+                
+                else
+                {
+                    for (int j = i; j > nextPosition; --j)
+                    {
+                        nums[j] = nums[j - 1];
+                    }
+                    
+                    nums[nextPosition] = nextElement;
+                }
+            }
+            
+            
+            return false;
+        }
+
+        public static bool Solution2(int[] nums) {
+
+            List<int> prevs = new List<int>();
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (prevs.Contains(nums[i])) return true;
+
+                else
+                    prevs.Add(nums[i]);
+            }
+
+
+            return false;
+        }
+
+        public static bool Solution3(int[] nums) {
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 1; ++i)
+            {
+                if (nums[i] == nums[i + 1]) return true;
+            }
+
+            return false;
+        }
+
+        public static bool Solution4(int[] nums) {
+
+            HashSet<int> h = new HashSet<int>(nums);
+
+            return (h.Count < nums.Length);
+        }
+    }
+}
---
+++ Maximum-Subarray/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+    Given an integer array nums, find the contiguous subarray (containing at least one number)
+    which has the largest sum and return its sum.
+
+    https://leetcode.com/problems/maximum-subarray/
+*/
+
+using System;
+
+namespace Maximum_Subarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+            int[] test2 = {1};
+            int[] test3 = {5, 4, -1, 7, 8};
+
+            int result = MaxSubArray(test);
+
+            Console.WriteLine($"Result for test1: {result}");
+
+            result = MaxSubArray(test2);
+            Console.WriteLine($"Result for test2: {result}");
+
+            result = MaxSubArray(test3);
+            Console.WriteLine($"Result for test3: {result}");
+        }
+
+        public static int MaxSubArray(int[] nums) {
+
+            int maxSum = int.MinValue;
+            int currentSum = 0;
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                currentSum += nums[i];
+
+                maxSum = Math.Max(maxSum, currentSum);
+
+                currentSum = Math.Max(0, currentSum);
+            }
+
+            return maxSum;
+        }
+
+    }
+}
