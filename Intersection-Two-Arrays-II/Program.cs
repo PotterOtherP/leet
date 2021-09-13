a48465ec7c6ba62462a2f7688b47e77e52a16570
Nate Tryon <natetryon@gmail.com>
2021-09-13 07:21:06 -0400

    Longest Palindromic Substring solution

---
+++ Intersection-Two-Arrays-II/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+    Given two integer arrays nums1 and nums2, return an array of their intersection.
+    Each element in the result must appear as many times as it shows in both arrays
+    and you may return the result in any order.
+
+    https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Intersection_Two_Arrays_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = {4, 7, 9, 6, 7};
+            int[] nums2 = {5, 0, 0, 6, 1, 6, 2, 2, 4};
+
+            int[] output = Intersect(nums1, nums2);
+
+            PrintArray(output);
+        }
+
+        public static int[] Intersect(int[] nums1, int[] nums2) {
+
+            if (nums1.Length <= nums2.Length)
+            {
+                return IntersectSmaller(nums1, nums2);
+            }
+
+            return IntersectSmaller(nums2, nums1);
+        }
+
+        public static int[] IntersectSmaller(int[] nums1, int[] nums2) {
+
+            List<int> list1 = new List<int>(nums1);
+            List<int> list2 = new List<int>(nums2);
+
+            PrintArray(list1.ToArray());
+            PrintArray(list2.ToArray());
+
+            // list1.Sort();
+            // list2.Sort();
+
+            for (int i = 0; i < list1.Count; ++i)
+            {
+                int x = list1[i];
+                if (list2.Contains(x))
+                {
+                    list2.Remove(x);
+                }
+
+                else
+                {
+                    list1.Remove(x);
+                    --i;
+                }
+            }
+
+            return list1.ToArray();
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            Console.Write("[");
+            for (int i = 0; i < arr.Length - 1; ++i)
+            {
+                int next = arr[i];
+                Console.Write($"{next}, ");
+            }
+            Console.Write(arr[arr.Length - 1]);
+            Console.Write("]\n");
+        }
+    }
+}
---
+++ Longest-Palindromic-Substring/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Longest_Palindromic_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "bb";
+
+            string answer = LongestPalindrome(str);
+            Console.WriteLine($"Answer: {answer}");
+        }
+
+        /*
+         * Solution: iterate over every possible palindrome center
+         */
+        public static string LongestPalindrome(string s) {
+
+            if (s.Length <= 1) return s;
+
+            string longestPal = s.Substring(0,1);
+
+
+            for (int i = 0; i < (s.Length * 2) - 3; ++i)
+            {
+                int l = i / 2;
+                int r = l + 1;
+
+                if (i % 2 == 1)
+                {
+                    l = (i / 2) + 1;
+                    r = l;
+                }
+
+                while (l >= 0 && r < s.Length && s[l] == s[r])
+                {
+                    if (s[l] == s[r] && s.Substring(l, r - l + 1).Length > longestPal.Length)
+                    {
+                        longestPal = s.Substring(l, r - l + 1);
+                    }
+                    
+                    --l;
+                    ++r;
+                }
+            }
+
+            
+
+            return longestPal;
+        }
+
+        /**
+         * This method checks every substring in
+         * reverse order of length. (Takes too long, O(n^3))
+         */
+        public static string LongestPalindromeBF(string s) {
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                int subLength = s.Length - i;
+                Console.WriteLine($"subLength is {subLength}");
+
+                for (int j = 0; j <= s.Length - subLength; ++j)
+                {
+                    string subStr = s.Substring(j, subLength);
+                    Console.WriteLine(subStr);
+                    if (CheckPalindrome(subStr))
+                        return subStr;
+                }
+            }
+
+
+            return s;
+        }
+
+
+        // Helper method
+        public static bool CheckPalindrome(string s) {
+
+            if (s.Length <= 1) return true;
+
+            for (int i = 0; i < s.Length / 2; ++i)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
